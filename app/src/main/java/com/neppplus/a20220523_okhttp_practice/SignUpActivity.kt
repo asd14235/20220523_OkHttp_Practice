32e6b4103c1d1c279a1b4be357a0c552abf300ca
조경진 <cho881020@gmail.com>
2022-05-23 16:48:35 +0900

    회원 가입 기능 작성 완료

--- app/src/main/java/com/neppplus/a20220523_okhttp_practice/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/a20220523_okhttp_practice/SignUpActivity.kt
@@ -2,8 +2,12 @@ package com.neppplus.a20220523_okhttp_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.neppplus.a20220523_okhttp_practice.databinding.ActivitySignUpBinding
+import com.neppplus.a20220523_okhttp_practice.utils.ServerUtil
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
 
@@ -19,6 +23,36 @@ class SignUpActivity : BaseActivity() {
     override fun setupEvents() {
         binding.signUpBtn.setOnClickListener { 
 //            회원가입을 함수가 실행
+
+            val inputEmail = binding.emailEdt.text.toString()
+            val inputPw = binding.passwordEdt.text.toString()
+            val inputNickname = binding.nicknameEdt.text.toString()
+
+            ServerUtil.putRequestSignUp(inputEmail, inputPw, inputNickname, object : ServerUtil.Companion.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+
+                    Log.d("inputEmail", inputEmail)
+
+                    val code = jsonObj.getInt("code")
+
+                    if (code == 200) {
+                        val dataObj = jsonObj.getJSONObject("data")
+                        val userObj = dataObj.getJSONObject("user")
+                        val nickname = userObj.getString("nick_name")
+
+                        runOnUiThread {
+                            Toast.makeText(mContext, "${nickname}님 가입을 환영합니다.", Toast.LENGTH_SHORT).show()
+                            finish()
+                        }
+                    }
+                    else {
+                        val message = jsonObj.getString("message")
+                        runOnUiThread {
+                            Toast.makeText(mContext, "실패사유 : $message", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            })
         }
     }
 
